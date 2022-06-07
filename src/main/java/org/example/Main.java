93c450516ce5fdae35382fb9f27142dbaaa53879
kajela <titoyut56@gmail.com>
2022-06-07 01:41:48 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int p = 0;
+        while (p < 10) {
+            System.out.println(p);
+            p++;
+            if (p == 8) {
+                continue;
+            }
+    int [] grades ={12,24,24};
+            System.out.println(grades[1]);
+            String [] names ={"tito", "kajela","to-the-world"};
+            names[2] = "muchoki";
+            System.out.println(names[1]);
+        }
     }
 }
